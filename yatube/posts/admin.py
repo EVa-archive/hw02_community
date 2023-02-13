0961a75086dc9e3778495e50ac61f01de7b4fae8
Елена Василькова <vasilkovaea@gmail.com>
2023-02-13 19:18:35 +0300

    Bugs fixed

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -2,19 +2,20 @@ from django.contrib import admin
 
 from .models import Post, Group
 
+
 class PostAdmin(admin.ModelAdmin):
     # Перечисляем поля, которые должны отображаться в админке
     list_display = (
-                    'pk', 
-                    'text', 
-                    'pub_date', 
+                    'pk',
+                    'text',
+                    'pub_date',
                     'author',
                     'group'
-                    ) 
+                    )
     # Добавляем интерфейс для поиска по тексту постов
-    search_fields = ('text',) 
+    search_fields = ('text',)
     # Добавляем возможность фильтрации по дате
-    list_filter = ('pub_date',) 
+    list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
     list_editable = ('group',)
 
